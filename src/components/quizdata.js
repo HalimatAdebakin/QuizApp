34973ea213399081586a78fa2d35a175d9e2bad3
HalimatAdebakin <adebakinhalimat@gmail.com>
2023-09-05 13:00:23 +0100

    first commit

---
+++ src/components/quizdata.js
@@ -0,0 +1,63 @@
+export const quizData = [
+    {
+      id: 1,
+      question: `What is the capital city of Canada ?`,
+      options: [`Toronto`, `Mumbai`, `Kolkatta`],
+      answer: `Toronto`
+    },
+    {
+      id: 2,
+      question: `Who won the 2014 world cup ?`,
+      options: [`Germany`, `Spain`, `Brazil`],
+      answer: `Germany`
+    },
+    {
+      id: 3,   
+      question: `Who is the World's Richest Man ?`,
+      options: [`Jeff Bezos`, `Bill Gates`, `Mark Zuckerberg`],
+      answer: `Jeff Bezos`
+    },
+    {
+      id: 4,
+      question: `World's Longest River ?`,
+      options: [`River Nile`, `River Amazon`, `River Godavari`],
+      answer: `River Nile`
+    },
+    {
+      id: 5,
+      question: `United States President ?`,
+      options: [`Barack Obama`, `Joe Biden`, `Kanye West`],
+      answer: `Joe Biden`
+    },
+    {
+      id: 6,
+      question: `Most Populated Country ?`,
+      options: [`China`, `India`, `Saudi Arabia`],
+      answer: `China`
+    },
+    {
+      id: 7,
+      question: `The wold largest Animal is ?`,
+      options: [`Anaconda`, `Lion`, `Blue Whale`],
+      answer: `Blue Whale`
+    },
+    {
+      id: 8,
+      question: `Largest planet is what ?`,
+      options: [ `Jupiter` , `Mars`, `Earth`],
+      answer: `Jupi`
+    },
+    {
+      id: 9,
+      question: `What is the capital of South-Africa ?`,
+      options: [`Cape Town`, `Cape Verde`, `Johannesburg`],
+      answer: `Johannesburg`
+    },
+    {
+      id: 10,
+      question: `What is the capital of Nigeria ?`,
+      options: [`Abuja`, `Cape Verde`, `PH City`],
+      answer: `Abuja`
+    },
+  ];
+  
